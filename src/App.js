f8aeb42232f833c74d24323f6e5704e285482d07
Sudeep Dahal <sudeepdahal75@gmail.com>
2022-08-31 20:25:16 +0545

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import Analytics from "./components/Analytics";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    
+      <div>
+        <Navbar />
+        <Hero />
+        <Analytics/>
+        
+      </div>
+ 
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Typed from 'react-typed';
+
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p className='text-green-400 font-bold p-2'>LEARNING WITH SUDEEP</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Sudeep is Awesome </h1>
+                <div className='flex justify-center items-center'>  
+        <p className='md:text-5xl sm:text-4xl font-bold py-4'> Talented, Reliable and  </p>
+        <Typed  className='md:text-5xl sm:text-4xl font-bold pl-2 md:pl-4'
+        strings={['Dami','Cool']} 
+        typeSpeed={120} 
+        backSpeed={120} loop/>
+ 
+        </div>
+        <button className='bg-green-400 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Get Started</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Hero
