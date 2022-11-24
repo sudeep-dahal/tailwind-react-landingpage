f5972f553b6ede59f3a65cca6801a1cecb1c2925
Sudeep Dahal <sudeepdahal75@gmail.com>
2022-11-24 10:00:04 +0545

    Final commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,57 +1,71 @@
-import React from 'react'
-import Single from '../assets/single.png'
-import Double from '../assets/double.png'
-import Triple from '../assets/triple.png'
+import React from "react";
+import Single from "../assets/single.png";
+import Double from "../assets/double.png";
+import Triple from "../assets/triple.png";
 
 const Cards = () => {
   return (
-    <div className='w-full py-[10rem] px-4 bg-white'>
-            <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
-                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="" />
-                    <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-                    <p className='text-center text-4xl font-bold'>Rs 2000</p>
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Single}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
+          <p className="text-center text-4xl font-bold">Rs 2000</p>
 
-                    <div className='text-center font-medium'>
-                        <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                        <p className='py-2 border-b mx-8'>1 Granted User</p>
-                        <p className='py-2 border-b mx-8'>Send upto 1 GB</p>
-                    </div> 
-                    <button className='bg-green-400 w-[200px] font-medium my-6 mx-auto px-6 py-3 rounded-md'>Start Trial</button>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send upto 1 GB</p>
+          </div>
+          <button className="bg-green-400 w-[200px] font-medium my-6 mx-auto px-6 py-3 rounded-md">
+            Start Trial
+          </button>
+        </div>
 
-                </div>
+        <div className="bg-gray-100 w-full shadow-xl flex flex-col p-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 bg-transparent mx-auto mt-[-3rem] bg-white"
+            src={Double}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Double User</h2>
+          <p className="text-center text-4xl font-bold">Rs 3500</p>
 
-                <div className='bg-gray-100 w-full shadow-xl flex flex-col p-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300'>
-                    <img className='w-20 bg-transparent mx-auto mt-[-3rem] bg-white' src={Double} alt="" />
-                    <h2 className='text-2xl font-bold text-center py-8'>Double User</h2>
-                    <p className='text-center text-4xl font-bold'>Rs 3500</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send upto 1 GB</p>
+          </div>
+          <button className="bg-black w-[200px] font-medium my-6 mx-auto px-6 py-3 rounded-md text-green-600">
+            Start Trial
+          </button>
+        </div>
 
-                    <div className='text-center font-medium'>
-                        <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                        <p className='py-2 border-b mx-8'>1 Granted User</p>
-                        <p className='py-2 border-b mx-8'>Send upto 1 GB</p>
-                    </div> 
-                    <button className='bg-black w-[200px] font-medium my-6 mx-auto px-6 py-3 rounded-md text-green-600'>Start Trial</button>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Triple}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Triple User</h2>
+          <p className="text-center text-4xl font-bold">Rs 6000</p>
 
-                </div>
-
-                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="" />
-                    <h2 className='text-2xl font-bold text-center py-8'>Triple User</h2>
-                    <p className='text-center text-4xl font-bold'>Rs 6000</p>
-
-                    <div className='text-center font-medium'>
-                        <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                        <p className='py-2 border-b mx-8'>1 Granted User</p>
-                        <p className='py-2 border-b mx-8'>Send upto 1 GB</p>
-                    </div> 
-                    <button className='bg-green-400 w-[200px] font-medium my-6 mx-auto px-6 py-3 rounded-md'>Start Trial</button>
-
-                </div>
-
-            </div>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send upto 1 GB</p>
+          </div>
+          <button className="bg-green-400 w-[200px] font-medium my-6 mx-auto px-6 py-3 rounded-md">
+            Start Trial
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import Typed from 'react-typed';
-
+import Typed from "react-typed";
 
 const Hero = () => {
   return (
-    <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='text-green-400 font-bold p-2'>VIBING WITH SUDEEP</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Sudeep is Awesome </h1>
-                <div className='flex justify-center items-center'>  
-        <p className='md:text-5xl sm:text-4xl font-bold py-4'> Talented, Reliable and  </p>
-        <Typed  className='md:text-5xl sm:text-4xl font-bold pl-2 md:pl-4'
-        strings={['Dami','Cool']} 
-        typeSpeed={120} 
-        backSpeed={120} loop/>
- 
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-green-400 font-bold p-2">VIBING WITH SUDEEP</p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Sudeep is Awesome{" "}
+        </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl font-bold py-4">
+            {" "}
+            Talented, Reliable and{" "}
+          </p>
+          <Typed
+            className="md:text-5xl sm:text-4xl font-bold pl-2 md:pl-4"
+            strings={["Dami", "Cool"]}
+            typeSpeed={120}
+            backSpeed={120}
+            loop
+          />
         </div>
-        <button className='bg-green-400 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Don't click me</button>
-        </div>
-        
+        <button className="bg-green-400 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black">
+          Don't click me
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
